cb7f647cd104f01840cb0a731dbc83deeb38a78d
unknown <rafik.boullaft@gmail.com>
2020-12-26 20:54:14 +0100

    intial commit for blood donation project

---
+++ src/main/java/com/ehealth/dondusang/DondusangApplication.java
@@ -0,0 +1,13 @@
+package com.ehealth.dondusang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DondusangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DondusangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ehealth/dondusang/DondusangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ehealth.dondusang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DondusangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
